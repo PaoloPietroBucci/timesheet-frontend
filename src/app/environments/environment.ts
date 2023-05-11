81012e187007de67e0081aa377b322dc3988db95
Paolo <p.bucci.wh@gmail.com>
2023-05-11 11:28:06 +0200

    server update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     images: 'https://pre-prod-images.waithero.com',
-    server: 'http://localhost:8080',
+    server: 'https://timesheet-be.amaggio.it',
     serverB2B: 'http://localhost:8080',
     requestToken: 'jkdlksalls-BKnknkdndklc'
   };
